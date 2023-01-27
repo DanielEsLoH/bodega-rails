43ed2663b08d58a066e302053fc9649fef3f106c
Daniel Londoño <daniel.esloh@gmail.com>
2023-01-26 21:00:22 -0500

    Proyecto finalizado

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,10 +4,23 @@ class ProductsController < ApplicationController
   # GET /products or /products.json
   def index
     @products = Product.all
+    respond_to do |format|
+      format.html
+      format.xlsx{
+        response.headers['content-Disposition'] = 'attachment; filename="Listado de productos.xlsx"'
+      }
+    end
   end
 
   # GET /products/1 or /products/1.json
   def show
+    @products = Product.all
+    respond_to do |format|
+      format.html
+      format.xlsx{
+        response.headers['content-Disposition'] = 'attachment; filename="Detalle de producto.xlsx"'
+      }
+    end
   end
 
   # GET /products/new
